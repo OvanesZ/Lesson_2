ee5ee57c46d96c4746b40c6f29ccf8e44e23afa1
Ovanes <o-zakharyan@mail.ru>
2021-01-02 13:24:57 -0800

    task_1

---
+++ Lesson_2/Lesson_2/main.swift
@@ -0,0 +1,20 @@
+//
+//  main.swift
+//  Lesson_2
+//
+//  Created by Ованес on 02.01.2021.
+//
+
+import Foundation
+
+
+func even(num1 a: Int){
+    if a % 2 == 0{
+        print("Число четное")
+    }else {
+        print("Число нечетное")
+    }
+}
+
+even(num1: 17)
+
